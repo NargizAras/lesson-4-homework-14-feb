e8c650f590841aea63208b169d0d5840dfc30758
NargizAras <nargiz.ismailova.1994@mail.ru>
2024-03-09 23:20:38 +0400

    first commit

---
+++ index.js1/index.js
@@ -0,0 +1,20 @@
+// let number1 = parseInt(prompt("Введите число"));
+// let number2 = parseInt(prompt("Введите число"));
+// let operator = prompt("Введите оператор");
+
+// switch (operator) {
+//   case "+":
+//     alert(number1 + number2);
+//     break;
+//   case "-":
+//     alert(number1 - number2);
+//     break;
+//   case "/":
+//     alert(number1 / number2);
+//     break;
+//   case "*":
+//     alert(number1 * number2);
+//     break;
+//   default:
+//     alert("введено неверное значение");
+// }
---
+++ index.js2/index.js
@@ -0,0 +1,21 @@
+// let fruit = prompt("Введите название фрукта").toLowerCase;
+
+// switch (fruit) {
+//   case "apple":
+//     alert("This is fruit");
+//     break;
+//   case "apricot":
+//     alert("This is fruit");
+//     break;
+//   case "pear":
+//     alert("This is fruit");
+//     break;
+//   case "cherry":
+//     alert("This is fruit");
+//     break;
+//   case "peach":
+//     alert("This is fruit");
+//     break;
+//   default:
+//     alert("This is not fruit");
+// }
